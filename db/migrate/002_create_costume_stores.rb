c9c5030ac230862976cec306f7f796e60d320e7f
Ming Duan <11203duan@gmail.com>
2019-05-12 20:36:41 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,6 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
+      t.datetime :opening_time
       t.timestamps
     end
 

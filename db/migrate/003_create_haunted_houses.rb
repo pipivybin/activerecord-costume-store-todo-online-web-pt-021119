c5af45828fb63480e5e508364a2e9952e0faa2ba
Ming Duan <11203duan@gmail.com>
2019-05-12 20:38:44 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,8 +1,7 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
 
-  def change
-    
+  def change  
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location

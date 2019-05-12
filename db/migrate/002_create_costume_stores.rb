f29b76a16cc25cb3a0cb07fceb784658ce069cb0
Ming Duan <11203duan@gmail.com>
2019-05-12 20:35:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :status
+      t.boolean :still_in_business
       t.timestamps
     end
 
